5eec91fd806a71bc8e8e2256a4018164246b8259
sri26mathi <67379858+sri26mathi@users.noreply.github.com>
2020-07-28 23:09:18 +0530

    c prog for grading

---
+++ grading.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+  int n;
+  printf("Enter the marks");
+  scanf("%d",n);
+  if(n>=85 && n<=100)
+   printf("Grade A");
+  if else(n>=70 && n<85)
+   printf("Grade B");
+  if else(n>=55 && n<70)
+   printf("Grade C");
+  if else(n>=40 && n<55)
+   printf("Grade D");
+  if else(n<40)
+   printf("Grade F");
+  return 0;
+  }
+  
